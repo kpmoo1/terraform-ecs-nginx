fe038efadddf348b2aa4184afa9c840053612761
kpmoo1 <102037864+kpmoo1@users.noreply.github.com>
2023-05-01 06:32:03 +1000

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
-# Providing a reference to our default VPC
+# creating reference to default VPC
 resource "aws_default_vpc" "default_vpc" {
 }
 
-# Providing a reference to our default subnets
+# creating reference to default subnets
 resource "aws_default_subnet" "default_subnet_a" {
   availability_zone = "ap-southeast-2a"
 }
